7d3b4cacb1a0af6ff50d58e36611fc8b1d50de0b
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2025-11-15 13:49:26 -0300

    Teste

--- mdgpt/app/page.tsx
+++ mdgpt/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   const apiKey =
     process.env.NEXT_PUBLIC_OPENAI_API_KEY ?? process.env.OPENAI_API_KEY ?? "";
 
+  console.log("env", process.env);
+  console.log("apiKey", apiKey);
+
   return (
     <div className="flex flex-col h-full">
       <main className="flex-1 flex flex-col overflow-hidden">
