c319bda47cf3391c22cfff024baebdfa1975b0ae
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2025-11-15 13:30:03 -0300

    teste

--- mdgpt/app/page.tsx
+++ mdgpt/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   const apiKey =
     process.env.NEXT_PUBLIC_OPENAI_API_KEY ?? process.env.OPENAI_API_KEY ?? "";
 
+  console.log(process.env);
+
   return (
     <div className="flex flex-col h-full">
-      <main className="flex-1 flex flex-col overflow-hidden">
-        <ChatInterface apiKey={apiKey} />
-      </main>
+      <main className="flex-1 flex flex-col overflow-hidden">dsadasdsa</main>
     </div>
   );
 }
