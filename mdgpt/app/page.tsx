ef33c6ca362c04ffdf0bf01999212cb93de3e63b
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2025-11-15 16:43:23 -0300

    Ajuste

--- mdgpt/app/page.tsx
+++ mdgpt/app/page.tsx
@@ -5,14 +5,9 @@ import ChatInterface from "./components/ChatInterface";
 export default function Home() {
   const apiKey =
     process.env.NEXT_PUBLIC_OPENAI_API_KEY ?? process.env.OPENAI_API_KEY ?? "";
-
-  console.log("env", process.env.ENV_VAR);
-  console.log("apiKey", apiKey);
-
   return (
     <div className="flex flex-col h-full">
       <main className="flex-1 flex flex-col overflow-hidden">
-        <p>AQUI: {process.env.ENV_VAR}</p>
         <ChatInterface apiKey={apiKey} />
       </main>
     </div>
